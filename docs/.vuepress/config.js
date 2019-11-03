280f6f27a4418db1ad084db2d979fef2e9b8df55
zhaoguojun <985531883@qq.com>
2019-11-03 22:48:03 +0800

    deploy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: '大俊的知识库',
+  description: '大俊的博客',
+  head: [
+    ['link', { rel: 'icon', href: '/image/avator.png'}]
+  ],
+  themeConfig: {
+    // 导航栏配置
+    nav: [
+        { text: '主页', link: '/' },
+        { text: '博文', link: '/android/'
+          // items: [
+          //   { text: 'Android', link: '/android/' },
+          //   { text: 'ios', link: '/ios/' },
+          //   { text: 'Web', link: '/web/' }
+          // ]
+        },
+        { text: '关于', link: '/about/' },
+        { text: 'Github', link: 'https://www.github.com/kkxiaojun' },
+    ],
+    displayAllHeaders: true,
+    sidebar: 'auto',
+    lastUpdated: 'Last Updated', 
+  }
+}
