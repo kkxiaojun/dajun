3fa7ad5d92e4ab73a4715c939f452732d17f4e97
kkxiaojun <985531883@qq.com>
2019-12-04 11:18:22 +0800

    link

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,5 @@ git commit -m 'deploy'
 
 # 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:kkxiaojun/dajun.git master:gh-pages
-
+# git push -f https://github.com/kkxiaojun/dajun.git master:gh-pages
 cd -
